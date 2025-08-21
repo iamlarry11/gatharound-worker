9e0af8f265a3750b2d621cd9e04e4837c6b00b01
Alan Matviienko <matthewsjdw@gmail.com>
2025-08-22 01:08:19 +0200

    Add email deeplink redirect

--- src/index.js
+++ src/index.js
@@ -2,12 +2,22 @@ const AASA_JSON = '{"applinks":{"details":[{"appID":"K4CWFUNVVK.com.appgatharoun
 
 export default {
   async fetch(request) {
-    const { pathname } = new URL(request.url);
+    const url = new URL(request.url);
+    const { pathname, search } = url;
+
+    // AASA файл
     if (pathname === "/.well-known/apple-app-site-association") {
       return new Response(AASA_JSON, {
         headers: { "Content-Type": "application/json" }
       });
     }
+
+    // редирект для emailConfirm
+    if (pathname.startsWith("/emailConfirm")) {
+      const deepLink = `gatharound://emailConfirm${search}`;
+      return Response.redirect(deepLink, 302);
+    }
+
     return new Response("Not found", { status: 404 });
   }
 };
