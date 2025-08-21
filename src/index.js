272558b7d0ef66ec47b00c74b9eea2cfc84756bd
Alan Matviienko <matthewsjdw@gmail.com>
2025-08-22 01:28:43 +0200

    Test

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const AASA_JSON = '{"applinks":{"details":[{"appID":"K4CWFUNVVK.com.appgatharound.app","paths":["/recipes/*","/invites/*","/emailConfirm*"]}]},"webcredentials":{"apps":["K4CWFUNVVK.com.appgatharound.app"]}}';
+const AASA_JSON = '{"applinks":{"details":[{"appID":"K4CWFUNVVK.com.appgatharound.app","paths":["/recipes/*","/invites/*","/emailConfirm*","/test*"]}]},"webcredentials":{"apps":["K4CWFUNVVK.com.appgatharound.app"]}}';
 
 export default {
   async fetch(request) {
@@ -11,32 +11,32 @@ export default {
         headers: { "Content-Type": "application/json" }
       });
     }
-
+    
+    // AASA файл
     if (pathname === "/test") {
-      return new Response('{"test":"1"', {
+      return new Response('{"test":"details"}', {
         headers: { "Content-Type": "application/json" }
       });
     }
+
     // редирект для emailConfirm
     if (pathname.startsWith("/emailConfirm")) {
-      const base = "gatharound://emailConfirm";
-      const statusCode = 301; // или 301 если хочешь постоянный
-      const destinationURL = `${base}${search}`;
-      return Response.redirect(destinationURL, statusCode);
+      const deepLink = `gatharound://emailConfirm${search}`;
+      return Response.redirect(deepLink, 301);
     }
 
     // кастомная 404
     return new Response(
-      '
+      `
       <html>
         <head><title>Страница не найдена</title></head>
         <body style="font-family: sans-serif; text-align: center; margin-top: 50px;">
-          <h1>404</h1>
-          <p>Упс! Такой страницы нет.</p>
+          <h1>Упс!</h1>
+          <p>Такой страницы не существует.</p>
           <p><a href="https://gatharound.com">Вернуться на главную</a></p>
         </body>
       </html>
-      '
+      `,
       {
         status: 404,
         headers: { "Content-Type": "text/html; charset=utf-8" }
